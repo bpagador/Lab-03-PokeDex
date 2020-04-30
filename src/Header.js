591d0d4f572c219e4a06cf4e7f5334b3d85523c2
Briseid Pagador <bpagador@gmail.com>
2020-04-29 19:04:41 -0700

    show working site, needs data src from poke api

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className="header-container">
+                    <h1>PokeDex: Gotta Catch 'Em All</h1>
+                </div>
+            </header> 
+        );
+    }
+}
--- src/PokeItem.js
+++ src/PokeItem.js
@@ -4,10 +4,11 @@ export default class PokeItem extends Component {
     render() {
         return (
             <li className = 'item-container'>
-                <img src={this.props.pokeData.url_image} alt={this.props.pokeData.pokemon} />
-                <p>Attack: {this.props.pokeData.attack}; Defense: {this.props.pokeData.defense}</p>
                 <h2>{this.props.pokeData.pokemon}</h2>
+                <img src={this.props.pokeData.url_image} alt={this.props.pokeData.pokemon} />
+                <p>Attack: {this.props.pokeData.attack} | Defense: {this.props.pokeData.defense}</p>
             </li>
+
         );
     }
 }
